339e97c969a9a9c45b34514a16aa2dd6c403056e
Liguang <rencai2010@126.com>
2017-06-19 08:36:13 +0800

    first commit

---
+++ src/components/common/leftNav.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="left-nav">
+     <ul>
+         <li>
+             <i class="icon iconfont icon-goumai"></i>
+             <p>收银</p>
+         </li>
+         <li>
+             <i class="icon iconfont icon-dianpu"></i>
+             <p>店铺</p>
+         </li>
+         <li>
+             <i class="icon iconfont icon-hanbao"></i>
+             <p>商品</p>
+         </li>
+         <li>
+             <i class="icon iconfont icon-vip"></i>
+             <p>会员</p>
+         </li>
+         <li>
+             <i class="icon iconfont icon-tongji"></i>
+             <p>统计</p>
+         </li>
+         <li>
+             <i class="icon iconfont icon-shezhi"></i>
+             <p>设置</p>
+         </li>
+     </ul>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  .left-nav{
+      color:#fff;
+      font-size: 10px;
+      height: 100%;
+      width: 5%;
+      background: #1D8CE0;
+      float: left;
+  }
+  .iconfont{
+      font-size: 24px;
+  }
+  .left-nav ul{
+      padding: 0;
+      margin: 0;
+  }
+  .left-nav ul li{
+      list-style: none;
+      text-align: center;
+      border-bottom: 1px solid #20a0ff;
+      padding:10px;
+  }
+</style>
+
+
